dfa7f4e7566379e4a934b9763e0a411b27bf20ad
EricZinda <ericz@inductorsoftware.com>
2022-09-08 11:41:52 -0700

    Add ability to process relative links properly

---
+++ createdocs.py
@@ -0,0 +1,7 @@
+# Given a definition of docs create the latestsrc folder
+# format is JSON
+{
+    "Site": "concept",
+    "SrcDir": "docswiki",
+    "SrcFile": "ErgSemantics.md",
+}
---
+++ processlinks.py
@@ -0,0 +1,5 @@
+# Start with a folder and recursively process every file:
+# - Parse the file and convert all relative links
+
+if __name__ == '__main__':
+    pass
