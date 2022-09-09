14f4596b8b9cf1a0307a045720103f0c2f01f163
EricZinda <ericz@inductorsoftware.com>
2022-09-09 13:47:43 -0700

    Print out proposed fixes for broken links

--- PrototypeTODO.md
+++ PrototypeTODO.md
@@ -15,7 +15,10 @@ Figure out templates using Jekyll and see if they meet our needs
     - Build TOC properly
       - It doesn't link properly if you click on it.  Need to strip ".md"? 
     - Some images are broken
-    - 
+      - Github camo is used: https://github.com/atmos/camo by default in WIKI, this doesn't happen on github pages
+      - https://github.blog/2014-01-28-proxying-user-images/
+      - https://github.com/sionide21/camo-client
+    
 - Goal Build the current docs using the new process
     - Do the build in a way that allows versioning
     - Include all the current stuff
--- createdocs.py
+++ createdocs.py
@@ -1,3 +1,4 @@
+import copy
 import json
 import os
 import shutil
@@ -13,12 +14,12 @@ from marko.renderer import Renderer
 from marko.inline import Link
 
 
-def convert_and_copy_doc(parser, src_file_path, dst_file_path):
+def convert_and_copy_doc(parser, file_definition, src_file_path, dst_file_path):
     file_name, file_extension = os.path.splitext(src_file_path)
     if file_extension.lower() == ".md":
         with open(src_file_path, "r") as txtFile:
             result = parser.parse(txtFile.read())
-            links = convert_child(src_file_path, result)
+            links = convert_child(file_definition, result)
 
         with open(dst_file_path, "w") as txtFile:
             final_result = parser.render(result)
@@ -33,18 +34,21 @@ def convert_and_copy_doc(parser, src_file_path, dst_file_path):
     return links
 
 
-def convert_child(docpath, node):
+def convert_child(file_definition, node):
     links = []
     if isinstance(node, Link):
-        links.append({"Doc": docpath, "Link": node.dest})
-        split_url = urllib.parse.urlsplit(node.dest)
-        if split_url.scheme == "" and split_url.netloc == "" and split_url.query == "" and split_url.fragment == "":
-            parts = split_url.path.split('/')
+        _, parts = get_parts_for_local_link(node.dest)
+        link_data = copy.deepcopy(file_definition)
+        link_data["Link"] = node.dest
+        link_data["LinkParts"] = parts
+        links.append(link_data)
+        if parts is not None:
             if len(parts) == 1 and parts[0] != "":
                 node.dest = "../" + node.dest
+
     elif hasattr(node, "children"):
         for child in node.children:
-            links += convert_child(docpath, child)
+            links += convert_child(file_definition, child)
 
     return links
 
@@ -65,13 +69,20 @@ def create_sites_src(src_root, dst_root, sites_definitions_path):
     with open(sites_definitions_path, "r") as txtFile:
         sites_definition = json.loads(txtFile.read())
         links = []
-        docs = []
+        docs = {}
         tocs = {}
         for fileDefinition in sites_definition["Sites"]:
             src_file = os.path.join(src_root, fileDefinition["SrcDir"], fileDefinition["SrcFile"])
             dst_file = os.path.join(dst_root, fileDefinition["Site"], fileDefinition["SrcFile"])
-            docs.append(dst_file)
-            links += convert_and_copy_doc(parser, src_file, dst_file)
+
+            # Remember this doc in the site so we can find broken links later
+            file_site = fileDefinition["Site"]
+            if fileDefinition["Site"] not in docs:
+                docs[file_site] = {}
+            path = get_site_relative_page_link_from_src(file_site, fileDefinition["SrcFile"])
+            docs[file_site][path] = copy.deepcopy(fileDefinition)
+
+            links += convert_and_copy_doc(parser, fileDefinition, src_file, dst_file)
             if fileDefinition["Site"] not in tocs:
                 tocs[fileDefinition["Site"]] = []
             site_relative_link = get_site_relative_page_link_from_src(fileDefinition["Site"], fileDefinition["SrcFile"])
@@ -110,15 +121,64 @@ def create_tocs(dst_root, tocs):
             txtFile.write(include_text)
 
 
+# returns the path and segments of a local link
+# returns None if this is not a local link
+def get_parts_for_local_link(link):
+    split_url = urllib.parse.urlsplit(link)
+    if split_url.scheme == "" and split_url.netloc == "" and split_url.query == "" and split_url.fragment == "":
+        return split_url.path, split_url.path.split('/')
+    else:
+        return None, None
+
+
+# Given the full set of links on all pages:
+# 1. Find the ones we think are broken
+# 2. Propose entries to sitedefinitions.json to fix them
+def propose_broken_links(all_pages, all_links):
+    broken_links = find_broken_links(all_pages, all_links)
+    proposals = []
+    for linkItem in broken_links.items():
+        link = linkItem[1]
+        proposals.append({"Site": link["Site"], "Section": link["Section"], "Page": link["LinkParts"][-1], "SrcDir": link["SrcDir"], "SrcFile": link["Link"]})
+
+    return proposals
+
+
+# all_links includes the link that was found in ["Link"] along with the entire record from sitesdefinitions.json
+def find_broken_links(all_pages, all_links):
+    broken_links = {}
+    for link_data in all_links:
+        local_path, local_path_parts = get_parts_for_local_link(link_data["Link"])
+        if local_path is not None:
+            # strip the "../" from the link since that is just a workaround
+            if local_path_parts[0] == "..":
+                local_path = local_path_parts[-1]
+                link_data["Link"] = local_path
+
+            # If a link is relative, see if it exists
+            link_site_pages = all_pages[link_data["Site"]]
+            if local_path not in link_site_pages:
+                # Check to ensure it is not already there
+                if link_data["Link"] not in broken_links:
+                    broken_links[link_data["Link"]] = link_data
+
+        else:
+            # TODO: If a link is to a fully qualified address, test to see if it works
+            pass
+
+    return broken_links
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 4:
         src_root = sys.argv[1]
         dst_root = sys.argv[2]
         sites_definitions_path = sys.argv[3]
-        docs, links, tocs = create_sites_src(src_root, dst_root, sites_definitions_path)
+        all_pages, all_links, tocs = create_sites_src(src_root, dst_root, sites_definitions_path)
         create_tocs(dst_root, tocs)
-        print(docs)
-        print(links)
+        proposed_fixes = propose_broken_links(all_pages, all_links)
+        for item in proposed_fixes:
+            print(f"{item},")
 
     else:
         print("Error: Requires 3 arguments: 1) full path to where repositories containing docs are stored, 2) full path to the latestsrc directory of the docs repository, 3) full path and filename of the json file that defines the docs")
