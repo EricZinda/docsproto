42b09759017c26c915544933116c8bc6ee66cb6f
EricZinda <ericz@inductorsoftware.com>
2022-09-13 13:22:53 -0700

    Clean out old prototype stuff

--- initial_concept/index.md
+++
@@ -1,4 +0,0 @@
----
-layout: delphin_page
----
-
--- processlinks.py
+++
@@ -1,5 +0,0 @@
-# Start with a folder and recursively process every file:
-# - Parse the file and convert all relative links
-
-if __name__ == '__main__':
-    pass
